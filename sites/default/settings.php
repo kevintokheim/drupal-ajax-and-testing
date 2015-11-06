663a9e0649ab4d7461eb72c2f1fd2b592ccc64e3
Kevin <katokh89@gmail.com>
2015-11-06 15:37:35 -0800

    finish with mymodule code, however links are not yet working.

--- sites/default/settings.php
+++ sites/default/settings.php
@@ -212,7 +212,21 @@
  *   );
  * @endcode
  */
-$databases = array();
+$databases = array (
+  'default' => 
+  array (
+    'default' => 
+    array (
+      'database' => 'ajax_database',
+      'username' => 'epicodus',
+      'password' => 'epicodus',
+      'host' => 'localhost',
+      'port' => '',
+      'driver' => 'mysql',
+      'prefix' => '',
+    ),
+  ),
+);
 
 /**
  * Access control for update.php script.
@@ -244,7 +258,7 @@ $update_free_access = FALSE;
  *   $drupal_hash_salt = file_get_contents('/home/example/salt.txt');
  *
  */
-$drupal_hash_salt = '';
+$drupal_hash_salt = '8t0ExbtPmRvkbeGnpfO5hAYUlPeRakipbB-gnb35h1I';
 
 /**
  * Base URL (optional).
